9a43dcf216346ecb9bebf7386a07d1ac9b06dd8c
vu.do <vu.do@saigontechnology.com>
2023-08-31 03:17:40 +0700

    add refresh token interceptor, remove generated folder

--- my_custom_templates/lib/kinde_api.dart
+++ my_custom_templates/lib/kinde_api.dart
@@ -5,6 +5,7 @@
 export 'package:kinde_flutter_sdk/src/api.dart';
 export 'package:kinde_flutter_sdk/src/auth/api_key_auth.dart';
 export 'package:kinde_flutter_sdk/src/auth/basic_auth.dart';
+export 'package:kinde_flutter_sdk/src/auth/bearer_auth.dart';
 export 'package:kinde_flutter_sdk/src/auth/oauth.dart';
 export 'package:kinde_flutter_sdk/src/serializers.dart';
 export 'package:kinde_flutter_sdk/src/model/date.dart';
